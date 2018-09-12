d7002d5a65186b220a297dd6fb8079e26c0acc6f
Xwilarg <xwilarg@yahoo.fr>
2018-09-12 10:44:15 +0200

    Add movements that weren't push with last commit

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    private const float speed = 3f;
+
+    private void Update()
+    {
+        transform.Translate(new Vector2(-speed, 0f) * Time.deltaTime);
+        if (transform.position.x < -27f)
+            transform.Translate(new Vector2(54f, 0f));
+    }
+}
---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    private const float speed = 10f;
+    private Camera cam;
+    [SerializeField]
+    private GameObject bulletPrefab;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        transform.Translate(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Time.deltaTime * speed);
+        if (Input.GetKeyDown(KeyCode.Space)) ;
+    }
+}
