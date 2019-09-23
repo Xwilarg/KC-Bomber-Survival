03ca22203197d03c741e210191be160b241d1182
Xwilarg <xwilarg@yahoo.fr>
2019-09-23 10:43:29 +0900

    Improve introduction scene

--- Assets/Scripts/MoveTowards.cs
+++ Assets/Scripts/MoveTowards.cs
@@ -1,23 +1,47 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class MoveTowards : MonoBehaviour
 {
     [SerializeField]
     private Vector2 target;
-    private const float speed = 2f;
-    private const float closeEnough = 0.05f;
+    [SerializeField]
+    private float timeBeforeStart;
+    [SerializeField]
+    private UnityEvent onComplete;
+    [SerializeField]
+    private bool lockX, lockY;
+    [SerializeField]
+    private float speed;
+    private float closeEnough;
+
+    private void Start()
+    {
+        closeEnough = speed * 0.1f;
+    }
 
     private void Update()
     {
+        if (timeBeforeStart > 0f)
+        {
+            timeBeforeStart -= Time.deltaTime;
+            return;
+        }
         float x = transform.position.x - target.x;
         float y = transform.position.y - target.y;
         int newX = 0, newY = 0;
 
-        if (x > closeEnough) newX = -1;
-        else if (x < -closeEnough) newX = 1;
+        if (!lockX)
+        {
+            if (x > closeEnough) newX = -1;
+            else if (x < -closeEnough) newX = 1;
+        }
 
-        if (y > closeEnough) newY = -1;
-        else if (y < -closeEnough) newY = 1;
+        if (!lockY)
+        {
+            if (y > closeEnough) newY = -1;
+            else if (y < -closeEnough) newY = 1;
+        }
 
         if (newX == 0 && newY == 0)
         {
@@ -26,7 +50,8 @@ public class MoveTowards : MonoBehaviour
                 cc.enabled = true;
             else
             {
-                GetComponent<EscortController>().enabled = true;
+                onComplete.Invoke();
+                GetComponent<MoveTowards>().enabled = false;
             }
             enabled = false;
         }
